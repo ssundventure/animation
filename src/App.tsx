077b3dbedb6395bce38c3d1be2cf13de47ce468b
ssundventure <ssundventure@gmail.com>
2025-02-01 01:13:12 +0900

    practice variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
-import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
+
 function App() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Box
+        initial={{ scale: 0, type: "spring" }}
+        transition={{ duration: "3" }}
+        animate={{ borderRadius: "100px", scale: 1, rotateZ: 360 }}
+      />
+
+      <Box variants={myVars} initial="start" animate="end" />
+    </Wrapper>
+  );
 }
 
 export default App;
